b7dddba2ba1805068d8c24b49721260f45722793
Colton Foremaster <notloc.codes@gmail.com>
2023-07-19 02:41:18 +0000

    Cleaned up unused code

--- internal/services/characterService.go
+++ internal/services/characterService.go
@@ -1,12 +1,13 @@
 package services
 
 import (
-	// "context"
+	"context"
+	"fmt"
 
 	"sw-game-datapad/internal/swData"
 	"sw-game-datapad/internal/vendor"
 	"sw-game-datapad/pkg/logger"
-	// "time"
+	"time"
 )
 
 type PlanetCall struct {
@@ -37,31 +38,7 @@ func NewCharacterService(vendorService vendor.VendorService) *characterService {
 	}
 }
 
-// Benchmarks
-// "l" with everything 6.49, 5.42, 4.73
-// "l" with planets ONLY 3.43, 2.27, 5.12
-// "p" with planets & species 15.1, 4.56, 4.58
-// "p" with everything 18.73, 17.44
 func (service *characterService) GetCharacters(query string) ([]swData.Character, error) {
-	if res, err := service.Vendor.GetCharacters(query); err != nil {
-		return nil, err
-	} else {
-
-		store := newCharacterDataStore(res)
-		service.populatePlanets(&store)
-		service.populateSpecies(&store)
-		service.populateStarships(&store)
-		store.PopulateCharacters()
-		return store.Characters(), nil
-	}
-}
-
-// Benchmarks
-// "l" with everything
-// "l" with planets ONLY
-// "p" with planets & species 1.6, 2.24, 2.0
-// "p" with everything 2.87, 4.0, 5.73
-func (service *characterService) GetCharacters2(query string) ([]swData.Character, error) {
 	if res, err := service.Vendor.GetCharacters(query); err != nil {
 		return nil, err
 	} else {
@@ -70,50 +47,43 @@ func (service *characterService) GetCharacters2(query string) ([]swData.Characte
 		starshipsCalls := make(chan StarshipsCall)
 
 		store := newCharacterDataStore(res)
-		service.populatePlanetsChannel(&store, planetCalls)
-		service.populateSpeciesChannel(&store, speciesCalls)
-		service.populateStarshipsChannel(&store, starshipsCalls)
+		service.populatePlanets(&store, planetCalls)
+		service.populateSpecies(&store, speciesCalls)
+		service.populateStarships(&store, starshipsCalls)
 
-		// ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		// defer cancel()
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
 
-		// var call vendor.Planet
 		totalCalls := len(store.Planets()) + len(store.Species()) + len(store.StarShips())
 		received := 0
 		for received < totalCalls {
 			select {
 			case call := <-planetCalls:
-				logger.Log(logger.LogLevelInfo, "received PLANET call", call)
 				received++
 				if call.Error == nil {
 					store.AddPlanet(call.Id, call.Response)
 				}
 			case call := <-speciesCalls:
-				logger.Log(logger.LogLevelInfo, "received SPECIES call", call)
 				received++
 				if call.Error == nil {
 					store.AddSpecies(call.Id, call.Response)
 				}
 			case call := <-starshipsCalls:
-				logger.Log(logger.LogLevelInfo, "received STARSHIP call", call)
 				received++
 				if call.Error == nil {
 					store.AddStarShip(call.Id, call.Response)
 				}
+			case <-ctx.Done():
+				errMsg := "vendor timed out"
+				logger.Log(logger.LogLevelError, errMsg)
+				return nil, fmt.Errorf(errMsg)
 			}
 		}
 		store.PopulateCharacters()
 		return store.Characters(), nil
 	}
 }
-func (service *characterService) populatePlanets(store *characterDataStore) {
-	for planetId := range store.Planets() {
-		if planet, err := service.Vendor.GetPlanet(planetId); err == nil {
-			store.AddPlanet(planetId, *planet)
-		}
-	}
-}
-func (service *characterService) populatePlanetsChannel(store *characterDataStore, calls chan PlanetCall) {
+func (service *characterService) populatePlanets(store *characterDataStore, calls chan PlanetCall) {
 	for planetId := range store.Planets() {
 		go func(id string) {
 			planet, err := service.Vendor.GetPlanet(id)
@@ -126,14 +96,7 @@ func (service *characterService) populatePlanetsChannel(store *characterDataStor
 
 	}
 }
-func (service *characterService) populateSpecies(store *characterDataStore) {
-	for speciesId := range store.Species() {
-		if species, err := service.Vendor.GetSpecies(speciesId); err == nil {
-			store.AddSpecies(speciesId, *species)
-		}
-	}
-}
-func (service *characterService) populateSpeciesChannel(store *characterDataStore, calls chan SpeciesCall) {
+func (service *characterService) populateSpecies(store *characterDataStore, calls chan SpeciesCall) {
 	for speciesId := range store.Species() {
 		go func(id string) {
 			species, err := service.Vendor.GetSpecies(id)
@@ -146,14 +109,7 @@ func (service *characterService) populateSpeciesChannel(store *characterDataStor
 	}
 }
 
-func (service *characterService) populateStarships(store *characterDataStore) {
-	for shipId := range store.StarShips() {
-		if starship, err := service.Vendor.GetStarShip(shipId); err == nil {
-			store.AddStarShip(shipId, *starship)
-		}
-	}
-}
-func (service *characterService) populateStarshipsChannel(store *characterDataStore, calls chan StarshipsCall) {
+func (service *characterService) populateStarships(store *characterDataStore, calls chan StarshipsCall) {
 	for speciesId := range store.StarShips() {
 		go func(id string) {
 			species, err := service.Vendor.GetStarShip(id)
